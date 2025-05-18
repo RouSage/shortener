fd7bfd032e006c19af05ac72935602eb0b68eafb
RouSage <rousage@protonmail.com>
2025-05-18 17:40:38 +0200

    Zerolog logger

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,14 +2,73 @@ package server
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
-	e.Use(middleware.Logger())
+
+	e.Use(middleware.RequestID())
+
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogLatency:       true,
+		LogProtocol:      true,
+		LogRemoteIP:      true,
+		LogHost:          true,
+		LogMethod:        true,
+		LogURI:           true,
+		LogURIPath:       false,
+		LogRoutePath:     true,
+		LogRequestID:     true,
+		LogReferer:       true,
+		LogUserAgent:     true,
+		LogStatus:        true,
+		LogError:         true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			logEvent := logger.Info()
+			if v.Error != nil {
+				logEvent = logger.Error()
+			}
+
+			logEvent.
+				Int64("latency", v.Latency.Milliseconds()).
+				Str("protocol", v.Protocol).
+				Str("remote_ip", v.RemoteIP).
+				Str("host", v.Host).
+				Str("method", v.Method).
+				Str("uri", v.URI).
+				Str("route", v.RoutePath).
+				Str("request_id", v.RequestID).
+				Str("referer", v.Referer).
+				Str("user_agent", v.UserAgent).
+				Int("status", v.Status).
+				Str("uri", v.URI).
+				Int("status", v.Status).
+				Str("content_length", v.ContentLength).
+				Int64("response_size", v.ResponseSize)
+
+			if v.Error != nil {
+				logEvent.
+					Err(v.Error).
+					Msg("")
+			} else {
+				logEvent.Msg("request")
+			}
+
+			return nil
+		},
+		Skipper: func(c echo.Context) bool {
+			return c.Request().URL.Path == "/health"
+		},
+	}))
+
 	e.Use(middleware.Recover())
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
