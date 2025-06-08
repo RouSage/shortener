b506b3cbc71a9f2c73ad36027a63093ec28c0fc7
RouSage <rousage@protonmail.com>
2025-06-08 16:27:29 +0200

    Short URL config

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,7 +1,10 @@
 package config
 
+import "github.com/rs/zerolog/log"
+
 type App struct {
-	Env string
+	Env            string
+	ShortUrlLength int
 }
 
 func loadAppConfig() (App, error) {
@@ -10,5 +13,16 @@ func loadAppConfig() (App, error) {
 		return App{}, err
 	}
 
-	return App{Env: env}, nil
+	shortUrlLength, err := getIntEnv("SHORT_URL_LENGTH")
+	if err != nil {
+		// default to 0 if the env var is not set
+		// it's ok to not set this value, the generator package will use a default value
+		log.Warn().Msg("SHORT_URL_LENGTH environment variables is not set, setting to 0")
+		shortUrlLength = 0
+	}
+
+	return App{
+		Env:            env,
+		ShortUrlLength: shortUrlLength,
+	}, nil
 }
