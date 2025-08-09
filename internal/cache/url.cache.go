50abd6d8625923e93b95e535f0ddd5230b949735
RouSage <rousage@protonmail.com>
2025-08-09 12:48:25 +0200

    URL cache test suite

--- internal/cache/url.cache.go
+++ internal/cache/url.cache.go
@@ -10,28 +10,28 @@ var (
 	defaultExpire = 24 * time.Hour
 )
 
-func (c *Cache) SetLongUrl(ctx context.Context, code, longUrl string) error {
-	key := c.getUrlKey(code)
+func (c *Cache) SetLongUrl(ctx context.Context, code, longUrl string) (key string, err error) {
+	key = c.getUrlKey(code)
 
 	if _, err := c.client.Set(ctx, key, longUrl); err != nil {
-		return err
+		return key, err
 	}
 	if _, err := c.client.Expire(ctx, key, defaultExpire); err != nil {
-		return err
+		return key, err
 	}
 
-	return nil
+	return key, nil
 }
 
 func (c *Cache) GetLongUrl(ctx context.Context, code string) (string, error) {
 	key := c.getUrlKey(code)
 
-	res, err := c.client.Get(ctx, key)
+	resp, err := c.client.Get(ctx, key)
 	if err != nil {
 		return "", err
 	}
 
-	return res.Value(), nil
+	return resp.Value(), nil
 }
 
 func (c *Cache) getUrlKey(code string) string {
