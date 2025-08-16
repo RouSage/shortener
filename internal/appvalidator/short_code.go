57923ea053f9f0ea2ed43673b8c6bb80dda96d1f
RouSage <rousage@protonmail.com>
2025-08-16 11:01:56 +0200

    Add alphabet comment

--- internal/appvalidator/short_code.go
+++ internal/appvalidator/short_code.go
@@ -6,6 +6,8 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+// Same symbols as go-nanoid supports
+// See: https://github.com/matoous/go-nanoid?tab=readme-ov-file#go-nanoid
 var shortCode = regexp.MustCompile(`^[a-zA-Z0-9_-]+$`)
 
 func ValidateShortCode(fl validator.FieldLevel) bool {
