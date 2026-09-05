3bd198a9f624232b353f2d656aa76b9cbe9ac4ee
RouSage <rousage@protonmail.com>
2026-09-05 14:55:48 +0200

    Add sql formatter config

--- internal/repository/queries/url.sql
+++ internal/repository/queries/url.sql
@@ -1,41 +1,41 @@
 -- name: CreateUrl :one
 INSERT INTO
-  urls (id, long_url, is_custom, user_id)
+    urls (id, long_url, is_custom, user_id)
 VALUES
-  ($1, $2, $3, $4)
+    ($1, $2, $3, $4)
 RETURNING
-  *;
+    *;
 
 -- name: GetUserUrls :many
 SELECT
-  id,
-  long_url,
-  created_at,
-  is_custom,
-  COUNT(*) OVER () as total_count
+    id,
+    long_url,
+    created_at,
+    is_custom,
+    COUNT(*) OVER () AS total_count
 FROM
-  urls
+    urls
 WHERE
-  user_id = $1
+    user_id = $1
 ORDER BY
-  created_at DESC
+    created_at DESC
 LIMIT
-  $2
+    $2
 OFFSET
-  $3;
+    $3;
 
 -- name: GetLongUrl :one
 SELECT
-  long_url
+    long_url
 FROM
-  urls
+    urls
 WHERE
-  id = $1
+    id = $1
 LIMIT
-  1;
+    1;
 
 -- name: DeleteUserURL :execrows
 DELETE FROM urls
 WHERE
-  id = $1
-  AND user_id = $2;
+    id = $1
+    AND user_id = $2;
